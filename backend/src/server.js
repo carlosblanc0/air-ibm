e883679c64e3f5f8c27dd8350d0e4a66f27039d0
Carlos A. Blanco <carlosbusinessinquiries@gmail.com>
2022-04-19 12:30:40 -0700

    Fix environment variables

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 
-require('dotenv').config()
 const port = process.env.SERVER_PORT || 8080
 const app = express()
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
 app.use(express.json())
 app.use(routes)

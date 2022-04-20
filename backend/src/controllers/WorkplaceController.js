3eb729b318f179eba311316860830013772481a5
Carlos A. Blanco <carlosbusinessinquiries@gmail.com>
2022-04-20 12:23:01 -0700

    Add upload and update routing

--- backend/src/controllers/WorkplaceController.js
+++ backend/src/controllers/WorkplaceController.js
@@ -1,5 +1,19 @@
+const Workplace = require("../models/Workplace");
+
 module.exports = {
   async store(req, res) {
-    return res.json({ ok: true });
+    const { filename } = req.file;
+    const { company, branch, price } = req.body;
+    const { user_id } = req.headers;
+
+    const workplace = await Workplace.create({
+      user: user_id,
+      thumbnail: filename,
+      company,
+      branch: branch.split(",").map(branch => branch.trim()),
+      price
+    });
+
+    return res.json({ workplace });
   },
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
-const express = require('express')
-const router = express.Router()
-const Auth = require('./controllers/AuthController')
-const Workplace = require('./controllers/WorkplaceController')
+const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+const router = express.Router();
+const Auth = require("./controllers/AuthController");
+const Workplace = require("./controllers/WorkplaceController");
 
-router.post('/auth', Auth.store);
-router.post('/workplace', Workplace.store);
+const upload = multer(uploadConfig);
 
-module.exports = router
+router.post("/auth", Auth.store);
+router.post("/workplace", upload.single('thumbnail'), Workplace.store);
+
+module.exports = router;

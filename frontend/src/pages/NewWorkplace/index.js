0887adf031d195652994107ba799dfc9cd84e7cc
Carlos A. Blanco <carlosbusinessinquiries@gmail.com>
2022-04-25 10:22:50 -0700

    Delete boilerplate, amend styling + functionality

--- frontend/src/pages/NewWorkplace/index.js
+++ frontend/src/pages/NewWorkplace/index.js
@@ -1,31 +1,36 @@
 import React, { useState } from "react";
+import upload from "../../assets/upload.svg";
 import "./styles.css";
 
-
 export default function NewWorkplace() {
-    const [company, setCompany] = useState('');
-    const [branch, setBranch] = useState('');
+  const [company, setCompany] = useState("");
+  const [branch, setBranch] = useState("");
 
-    function handleSubmit() {
-    }
+  function handleSubmit() {}
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="company">Company</label>
-            <input
-                id="company"
-                placeholder="IBM, Kendryl, or Regional Partner (e.g. Century Link)"
-                value={company}
-                onChange={event => setCompany(event.target.value)}
-            />
-            <label htmlFor="company">Branch</label>
-            <input
-                id="branch"
-                placeholder="Research, Cloud, or Consulting"
-                value={branch}
-                onChange={event => setBranch(event.target.value)}
-            />
-            <button type="submit" className="btn-g">Publish listing</button>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <label id="thumbnail">
+        <input type="file" />
+        <img src={upload} alt="Carbon Design - Upload icon" />
+      </label>
+      <label htmlFor="company">Company</label>
+      <input
+        id="company"
+        placeholder="IBM, Kendryl, or Regional Partner (e.g. Century Link)"
+        value={company}
+        onChange={(event) => setCompany(event.target.value)}
+      />
+      <label htmlFor="company">Branch</label>
+      <input
+        id="branch"
+        placeholder="Research, Cloud, or Consulting"
+        value={branch}
+        onChange={(event) => setBranch(event.target.value)}
+      />
+      <button type="submit" className="btn-g">
+        Publish listing
+      </button>
+    </form>
+  );
 }

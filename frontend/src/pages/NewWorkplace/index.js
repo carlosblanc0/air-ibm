8f8910c3b2e64bec332044a969c98a40b6da75e1
Carlos A. Blanco <carlosbusinessinquiries@gmail.com>
2022-04-25 11:07:31 -0700

    Add prevent dafault event

--- frontend/src/pages/NewWorkplace/index.js
+++ frontend/src/pages/NewWorkplace/index.js
@@ -1,17 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
+import API from "../../services/API";
 import upload from "../../assets/upload.svg";
 import "./styles.css";
 
-export default function NewWorkplace() {
+export default function NewWorkplace(history) {
+  const [thumbnail, setThumbnail] = useState(null);
   const [company, setCompany] = useState("");
   const [branch, setBranch] = useState("");
+  const thumbnailPreview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
 
-  function handleSubmit() {}
+  async function handleSubmit(event) {
+    const data = new FormData();
+    const user_id = localStorage.getItem("user");
+    event.preventDefault();
+    data.append("thumbnail", thumbnail);
+    data.append("company", company);
+    data.append("branch", branch);
+
+    await API.post("/workplace", data, {
+      headers: { user_id },
+    });
+
+    history.push("/dashboard");
+  }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label id="thumbnail">
-        <input type="file" />
+      <label
+        id="thumbnail"
+        style={{ backgroundImage: `url(${thumbnailPreview})` }}
+        className={thumbnailPreview ? `has-thumbnail` : ""}
+      >
+        <input
+          type="file"
+          onChange={(event) => setThumbnail(event.target.files[0])}
+        />
         <img src={upload} alt="Carbon Design - Upload icon" />
       </label>
       <label htmlFor="company">Company</label>
